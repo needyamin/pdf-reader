69baa4b587151f05b5d440c1687a9708759bf5b3
Md. Yamin Hossain <needyamin@gmail.com>
2025-07-16 02:35:03 +0600

    b

--- build.py
+++ build.py
@@ -2,42 +2,53 @@ import os
 import sys
 import subprocess
 
-REQUIREMENTS = [
-    'PyMuPDF',
-    'Pillow',
-    'pywin32',
-    'tk',
-]
-
 REQUIREMENTS_TXT = 'requirements.txt'
 MAIN_SCRIPT = 'pdfReader.py'
 ICON = 'icon.ico'
 EXE_NAME = 'PDFReader.exe'
 
-# Write requirements.txt if not present
-if not os.path.exists(REQUIREMENTS_TXT):
-    with open(REQUIREMENTS_TXT, 'w') as f:
-        for pkg in REQUIREMENTS:
-            f.write(pkg + '\n')
-    print(f'Created {REQUIREMENTS_TXT}')
-
-# Install requirements
-print('Installing requirements...')
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', REQUIREMENTS_TXT])
-
-# Build with PyInstaller
-print('Building executable with PyInstaller...')
-pyinstaller_args = [
-    'pyinstaller',
-    '--noconfirm',
-    '--onefile',
-    '--windowed',
-    f'--name={EXE_NAME.replace(".exe", "")}',
+REQUIREMENTS = [
+    'PyMuPDF',
+    'Pillow',
+    'pywin32',
+    'pystray',
 ]
-if os.path.exists(ICON):
-    pyinstaller_args.append(f'--icon={ICON}')
-pyinstaller_args.append(MAIN_SCRIPT)
 
-subprocess.check_call(pyinstaller_args)
+def ensure_requirements_txt():
+    if not os.path.exists(REQUIREMENTS_TXT):
+        with open(REQUIREMENTS_TXT, 'w') as f:
+            for pkg in REQUIREMENTS:
+                f.write(pkg + '\n')
+        print(f'Created {REQUIREMENTS_TXT}')
+    else:
+        print(f'{REQUIREMENTS_TXT} already exists.')
 
-print(f'Build complete! Find your .exe in the dist/ folder as {EXE_NAME}') 
+def install_requirements():
+    print('Installing requirements...')
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', REQUIREMENTS_TXT])
+    print('All requirements installed.')
+
+def build_exe():
+    if not os.path.exists(MAIN_SCRIPT):
+        print(f'ERROR: {MAIN_SCRIPT} not found. Please make sure your main script is present.')
+        return
+    print('Building .exe with PyInstaller...')
+    cmd = [
+        sys.executable, '-m', 'PyInstaller',
+        '--onefile', '--windowed',
+        f'--icon={ICON}' if os.path.exists(ICON) else '',
+        '--name', EXE_NAME.replace('.exe',''),
+        MAIN_SCRIPT
+    ]
+    # Remove empty args
+    cmd = [arg for arg in cmd if arg]
+    subprocess.check_call(cmd)
+    print(f'Build complete! Find your .exe in dist/{EXE_NAME}')
+
+def main():
+    ensure_requirements_txt()
+    install_requirements()
+    build_exe()
+
+if __name__ == '__main__':
+    main() 
